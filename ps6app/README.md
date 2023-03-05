4bf06db98acfceb1620238ca4251efb901fae1bd
rnarasayya <rohannarasayya@hotmail.com>
2023-03-04 22:09:36 -0800

    Fourth commit of ps6

--- ps6app/README.md
+++ ps6app/README.md
@@ -1,1 +1,6 @@
 # ps6_shiny_app
+This dataset is a monthly count of Seattle Public Library checkouts by title for physical and electronic items. The dataset begins with checkouts that occurred in April 2005 and includes data as recent as 2023.
+The first panel contains a brief explanation of the dataset and displays the first few rows.
+The second panel plots the three most popular types of media checked out over time. It includes two widgets. The first lets the user select whether or not they want to see a connecting line on the scatterplot. The second lets the user choose which types of media can be displayed.
+The third panel displays a table of the total number of checkouts where the widget allows the user to choose if they want to see total checkouts grouped my month or by year.
+The link to the app is  https://rohan1.shinyapps.io/ps6app/
--- ps6app/app.R
+++ ps6app/app.R
@@ -9,44 +9,106 @@
 
 library(shiny)
 library(tidyverse)
+library(compare)
 
 data <- read_delim("./Checkouts_by_Title.csv")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+    titlePanel("Seattle Public Library Checkouts"),
+    tabsetPanel(
+      tabPanel("About", p("This dataset includes a", em("monthly"),"count of Seattle Public Library checkouts by title for physical and electronic items."),
+               p("The dataset begins with checkouts that occurred in April 2005."),
+               p("We have", strong(nrow(data)), "rows of data and", strong(ncol(data)), "columns."),
+               p("Here are the first few rows of the dataset."),
+               mainPanel(tableOutput("headData"))),
+      tabPanel(
+        "Plots",
+        sidebarLayout(
+          sidebarPanel(
+            checkboxGroupInput("type",
+              "Which material type do you want?",
+              choices = list("BOOK", "VIDEODISC", "EBOOK"),
+              selected = list("BOOK", "VIDEODISC", "EBOOK")
+            ),
+            checkboxInput("display", "Display line", FALSE)
+          ),
+          mainPanel(plotOutput("popular"))
+        )
+      ),
+      tabPanel(
+        "Tables",
+        sidebarLayout(
+          sidebarPanel(
+            radioButtons("time",
+              "Do you want to see total checkouts by month or year?",
+              choices = list("CheckoutMonth", "CheckoutYear")
+            )
+          ),
+          mainPanel(tableOutput("totals"), textOutput("tablemessage"))
         )
     )
+  )
 )
-
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+    output$headData <- renderTable({head(data)})
+    
+    output$popular <- renderPlot({
+      if(input$display) {
+        data %>% filter(MaterialType %in% input$type, CheckoutYear != 2023) %>%
+        group_by(CheckoutYear, MaterialType) %>%
+        summarize(totalCheckouts = sum(Checkouts)) %>%
+        ggplot(aes(x=CheckoutYear, y=totalCheckouts)) +
+        geom_point(aes(color=MaterialType)) +
+        geom_line(aes(color=MaterialType)) +
+        labs(x = "Year", y = "Total Checkouts")
+      }
+      else {
+        data %>% filter(MaterialType %in% input$type, CheckoutYear != 2023) %>%
+          group_by(CheckoutYear, MaterialType) %>%
+          summarize(totalCheckouts = sum(Checkouts)) %>%
+          ggplot(aes(x=CheckoutYear, y=totalCheckouts)) +
+          geom_point(aes(color=MaterialType)) +
+          labs(x = "Year", y = "Total Checkouts")
+      }
+    })
+    
+    #output$plotmessage <- renderText({
+      #inputdf <- data.frame(input$type)
+      #listdf <- data.frame(list("BOOK", "VIDEODISC", "EBOOK"))
+      #if(all(inputdf == listdf)) {
+        #df <- data %>% filter(MaterialType %in% input$type, CheckoutYear != 2023)
+      #}
+      #paste("Time period is 2005-2022. We have ", nrow(df), "rows in this subset.")
+    #})
+    
+    output$totals <- renderTable({
+      if(input$time == "CheckoutMonth") {
+        df <- data %>% group_by(CheckoutMonth) %>%
+        summarize(totalCheckouts = sum(Checkouts)) %>%
+        arrange(desc(totalCheckouts))
+      }
+      else {
+        df <- data %>% group_by(CheckoutYear) %>%
+          summarize(totalCheckouts = sum(Checkouts)) %>%
+          arrange(desc(totalCheckouts))
+      }
+    })
+    
+    output$tablemessage <- renderText({
+      if(input$time == "CheckoutMonth") {
+        df <- data %>% group_by(CheckoutMonth) %>%
+          summarize(totalCheckouts = sum(Checkouts)) %>%
+          arrange(desc(totalCheckouts))
+        paste("We have ", nrow(df), " rows in this table.")
+      }
+      else {
+        df <- data %>% group_by(CheckoutYear) %>%
+          summarize(totalCheckouts = sum(Checkouts)) %>%
+          arrange(desc(totalCheckouts))
+        paste("We have ", nrow(df), " rows in this table.")
+      }
     })
 }
 
